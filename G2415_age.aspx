d3c92a9668ef0ce373e79348b112ef55c0f9fae0
Markus Mikkola <G2415@student.jamk.fi>
2013-09-19 10:17:22 +0300

    Kalenteri sivusto
    Toimii melkein jotenki.

---
+++ G2415_age.aspx
@@ -0,0 +1,9 @@
+﻿<%@ Page Title="Calendar" Language="C#" MasterPageFile="~/MasterPage.master" AutoEventWireup="true" CodeFile="G2415_age.aspx.cs" Inherits="G2415_age" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
+    <asp:Calendar ID="Calendar" runat="server" Width="621px"></asp:Calendar>
+</asp:Content>
+
+
---
+++ MasterPage.master.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class MasterPage : System.Web.UI.MasterPage
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        lblDate.Text = "Tänään on: " + DateTime.Now;
+        fart();
+    }
+
+    void fart()
+    {
+        Calendar cal = ((Calendar)ContentPlaceHolder1.FindControl("Calendar"));
+        lblSelectedDate.Text = "The selected date is " + cal.SelectedDate.ToShortDateString();
+    }
+}
