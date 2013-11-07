11e38e9d3061d9a57800e352296f7fbf221d6a41
Markus Mikkola <G2415@student.jamk.fi>
2013-11-07 10:58:33 +0200

    EF häpellys
    huehue

---
+++ EFHapellys/Default.aspx
@@ -0,0 +1,28 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>EF Hue</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:Button ID="btnGetIlmot" runat="server" Text="Hae kaikki ilmot" OnClick="btnGetIlmot_Click" />
+        <asp:TextBox ID="txtboxAsioid" runat="server"></asp:TextBox>
+        <asp:Button ID="btnAsioid" runat="server" Text="Hae AsioId:n mukaan" OnClick="btnAsioid_Click"></asp:Button>
+        <asp:Label ID="lblLasnaoloa" runat="server">...</asp:Label> <br/>
+        <asp:DropDownList ID="cmbStudents" runat="server"></asp:DropDownList>
+        <asp:GridView ID="gvData" runat="server"></asp:GridView>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; AsioID&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Etunimi&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Sukunimi<br />
+        <asp:TextBox ID="txtboxMyAsioid" runat="server"></asp:TextBox>
+        <asp:TextBox ID="txtboxEtunimi" runat="server"></asp:TextBox>
+        <asp:TextBox ID="txtboxSukunimi" runat="server"></asp:TextBox>
+        <br />
+        <asp:Button ID="btnIlmottaudu" runat="server" Text="btnIlmottaudu" OnClick="btnIlmottaudu_Click"/>
+        <br />
+    </div>
+    </form>
+</body>
+</html>
---
+++ EFHapellys/Default.aspx.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            DemoxOyEntities ctx = new DemoxOyEntities();
+            var opiskelijat = (from lo in ctx.lasnaolots
+                               where lo.course.StartsWith("IIO13200")
+                               orderby lo.asioid
+                               select lo.asioid).Distinct();
+            cmbStudents.DataSource = opiskelijat.ToList();
+            cmbStudents.DataBind();
+        }
+    }
+    protected void btnGetIlmot_Click(object sender, EventArgs e)
+    {
+        //haetaan ilmoittautumiset EF:n avulla
+        DemoxOyEntities ctx = new DemoxOyEntities();
+        //Kysely tuo kaiken tiedon entiteeteistä
+        /*var kysely = from lo in ctx.lasnaolots
+                     where lo.course.Contains("IIO13200")
+                     select lo;*/
+        //Kysely jolla haetaan asioid, sukunimi, etunimi ja pvm
+        var kysely = from lo in ctx.lasnaolots
+                     where lo.course.Contains("IIO13200")
+                     select new { Nimi = lo.lastname + " " + lo.firstname,
+                         Asioid = lo.asioid,
+                         Pvm = lo.date 
+                     };
+        gvData.DataSource = kysely.ToList();
+        gvData.DataBind();
+        lblLasnaoloa.Text = string.Format("Löytyi {0} läsnäoloa", kysely.Count());
+    }
+
+    protected void btnAsioid_Click(object sender, EventArgs e)
+    {
+        //haetaan annetun opiskelijan ilmot
+        string JalluPullo = txtboxAsioid.Text;
+
+        if (txtboxAsioid.Text == "")
+        {
+            JalluPullo = cmbStudents.Text;
+            txtboxAsioid.Text = JalluPullo;
+        }
+
+        DemoxOyEntities ctx = new DemoxOyEntities();
+        var oppilaanLasnaolot = from lo in ctx.lasnaolots
+                                where (lo.course.Contains("IIO13200") &
+                                    lo.asioid.Contains(JalluPullo))
+                                orderby lo.date
+                                select new
+                                {
+                                    Pvm = lo.date,
+                                    Nimi = lo.lastname + " " + lo.firstname
+                                };
+        gvData.DataSource = oppilaanLasnaolot.ToList();
+        gvData.DataBind();
+        if (oppilaanLasnaolot.Count() != 0)
+        {
+            lblLasnaoloa.Text = string.Format("Löytyi {0} läsnäoloa", oppilaanLasnaolot.Count());
+        }
+        else
+        {
+            lblLasnaoloa.Text = "Ei löytynyt ilmoittautumisia opiskelijalla " + txtboxAsioid.Text;
+        }
+    }
+    protected void btnIlmottaudu_Click(object sender, EventArgs e)
+    {
+        if (txtboxMyAsioid.Text.Length * txtboxEtunimi.Text.Length * txtboxSukunimi.Text.Length > 0)
+        {
+            DemoxOyEntities ctx = new DemoxOyEntities();
+            lasnaolot lasnaolo = new lasnaolot();
+            lasnaolo.asioid = txtboxMyAsioid.Text;
+            lasnaolo.course = "IIO13200";
+            lasnaolo.firstname = txtboxEtunimi.Text;
+            lasnaolo.lastname = txtboxSukunimi.Text;
+            lasnaolo.date = DateTime.Now;
+
+            //lisätään se kontekstiin
+            ctx.lasnaolots.Add(lasnaolo);
+            //tallennus kantaan
+            ctx.SaveChanges();
+        }
+    }
+}
